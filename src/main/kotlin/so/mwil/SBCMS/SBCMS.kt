330388a6257b668aef3ca0530fccc947d4479b50
Matthew Wilson <matt@mattcodes.co.uk>
2018-08-17 00:15:55 +0100

    initial commit

---
+++ src/main/kotlin/so/mwil/SBCMS/SBCMS.kt
@@ -0,0 +1,11 @@
+package so.mwil.SBCMS
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SBCMS
+
+fun main(args: Array<String>) {
+    runApplication<SBCMS>(*args)
+}
